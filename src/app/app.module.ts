f73ffa0fdf049921f6913e27fb5f390c1daad365
axilo2 <caputom2@gmail.com>
2022-03-25 20:21:25 +0100

    esercizio1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './componenti/user-container/user-container.component';
+import { OrderContainerComponent } from './componenti/order-container/order-container.component';
+import { UserListComponent } from './componenti/user-list/user-list.component';
+import { UserDetailComponent } from './componenti/user-detail/user-detail.component';
+import { UserSingleComponent } from './componenti/user-single/user-single.component';
+import { OrderListComponent } from './componenti/order-list/order-list.component';
+import { OrderSingleComponent } from './componenti/order-single/order-single.component';
+import { OrderDetailComponent } from './componenti/order-detail/order-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    OrderContainerComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserSingleComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    OrderDetailComponent
   ],
   imports: [
     BrowserModule
