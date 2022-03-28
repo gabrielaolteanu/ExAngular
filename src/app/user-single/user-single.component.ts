7284b411462a70f7c8c415e368fc3809b169d05e
axilo2 <caputom2@gmail.com>
2022-03-28 18:34:51 +0200

    commit

--- src/app/user-single/user-single.component.ts
+++ src/app/user-single/user-single.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'stream';
 import { User } from '../models/User';
 
 @Component({
