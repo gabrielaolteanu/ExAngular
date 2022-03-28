fb007640cdf03d5816e74f28e87fdc5fc0688a3b
axilo2 <caputom2@gmail.com>
2022-03-28 17:38:28 +0200

    esercizio-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserSingleComponent } from './user-single/user-single.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    UserSingleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/user-single/user-single.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../models/User';
+
+@Component({
+  selector: 'angapp-user-single',
+  templateUrl: './user-single.component.html',
+  styleUrls: ['./user-single.component.css']
+})
+export class UserSingleComponent implements OnInit {
+  @Input() user!: User
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
